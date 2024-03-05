82abf7a2269373df35471b395b6bd8b80878bc2c
huy2000vu <huy2000vu@gmail.com>
2024-03-05 14:22:33 -0800

    broadcast design

--- master.py
+++ master.py
@@ -73,11 +73,32 @@ def pymotwMulticastExample():
     
     
 
+def broadcast_message(message):
+    multicast_group = '224.3.29.71'
+    multicast_port = 10000
 
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set time-to-live for messages to reach multiple subnets
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    try:
+        # Send data to the multicast group
+        print(f"Sending message to the multicast group: {message}")
+        sock.sendto(message.encode(), (multicast_group, multicast_port))
+
+    finally:
+        # Close the socket
+        sock.close()
 
 
 
 
 
 if __name__ == '__main__':
+    print('Multicast protocol example')
     pymotwMulticastExample()
+    print('broadcast protocol design')
+    broadcast_message('Hello World')
