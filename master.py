14dd3ab5c92338f56c4ac7203f543a48465a85e6
EunivaDev <eunivadev@gmail.com>
2024-03-04 16:31:09 -0800

    fixed issues with master and node

--- master.py
+++ master.py
@@ -16,10 +16,11 @@ import struct
 import sys
 import threading
 
+#TESTING, may ermove
 def handleNodeConnection(conn, addr):
     print(f"Node connected: {addr} and {conn}")
 
-
+#TESTING, may remove
 def startServer():
     host = "0.0.0.0"
     port = 5000
@@ -37,5 +38,45 @@ def startServer():
 
 
 
+# from example
+def pymotwMulticastExample():
+    message = 'very important data'
+    multicastGroup = ('224.3.29.71', 10000)
+
+    #Create the diatagram socket
+    datagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set timeout so socket doesn't block indefinitely 
+    datagramSocket.settimeout(0.2)
+
+
+    ttl = struct.pack('b', 1)
+    datagramSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+
+    try:
+        print(f"sending {message}", file=sys.stderr)
+        sent = datagramSocket.sendto(message.encode(), multicastGroup)
+
+        while True:
+            print("waiting to receive", file=sys.stderr)
+            try:
+                data, server = datagramSocket.recvfrom(16)
+            except socket.timeout:
+                print("timed out, no more responses", file=sys.stderr)
+                break
+            else:
+                print(f"received {data} from {server}", file=sys.stderr)
+    finally:
+        print("closing socket", file=sys.stderr)
+    
+    
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    startServer()
+    pymotwMulticastExample()
--- node.py
+++ node.py
@@ -1,7 +1,10 @@
 # node.py
 
 import socket
+import struct
+import sys
 
+# temp
 def connectToMaster():
     host = "master"
     port = 5000
@@ -12,5 +15,30 @@ def connectToMaster():
     print(f"Connected to Master Server at {host}:{port}")
 
 
+def main():
+    multicastGroup = '224.3.29.71'
+    serverAddress = ('224.3.29.71', 10000)
+    
+    datagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    datagramSocket.bind(serverAddress)
+
+
+    group = socket.inet_aton(multicastGroup)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    datagramSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print("waiting to receive message", file=sys.stderr)
+        data, address = datagramSocket.recvfrom(1024)
+
+        print(f"received {len(data)} bytes from {address}",  file=sys.stderr)
+
+        print(data, file=sys.stderr)
+
+        print(f'sending acknowledgement to {address}', file=sys.stderr)
+        datagramSocket.sendto('ack'.encode(), address)
+
+
 if __name__ == "__main__":
-    connectToMaster()
+    main()
